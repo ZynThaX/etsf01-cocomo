c25196feca93f65f1b797a5723c11677d3418974
Jonas Jinbäck <jonasss.90@gmail.com>
2013-04-12 15:10:33 +0300

    Update cocomo2.js
    added new js formula

--- app/assets/javascripts/cocomo2.js
+++ app/assets/javascripts/cocomo2.js
@@ -1,11 +1,21 @@
 function calculate(){
   var result = 1;
 
+  val totalSF = 0.91;
+  val em = 1;
   $('select > option:selected').each(function() {
-      result = result*$(this).val()
+      totalSF = totalSF + 0.01*$(this).data("sf");
+      em = em*$(this).val();
   });
+  
+  val linesOfCode = $("#linesofcode").val()/1000;
+  val size = Math.pow(linesOfCode, totalSF);
+  
+  val pm = 2.94*size*em;
  
-  $("#organic").val((3.2*Math.pow($("#linesofcode").val()/1000, 1.05)*result).toFixed(2));
-  $("#semidetached").val((3.0*Math.pow($("#linesofcode").val()/1000, 1.12)*result).toFixed(2));
-  $("#embedded").val((2.8*Math.pow($("#linesofcode").val()/1000, 1.2)*result).toFixed(2));
+  alert(pm);
+ 
+ // $("#organic").val((3.2*Math.pow($("#linesofcode").val()/1000, 1.05)*result).toFixed(2));
+  //$("#semidetached").val((3.0*Math.pow($("#linesofcode").val()/1000, 1.12)*result).toFixed(2));
+ // $("#embedded").val((2.8*Math.pow($("#linesofcode").val()/1000, 1.2)*result).toFixed(2));
 }
